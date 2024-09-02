3df7ed4052973af0f211e3d255e624e8bb3fce5e
Omer Hamza <omer.hamza121996@gmail.com>
2024-09-02 16:30:25 +0500

    added requirements file

--- student/student/urls.py
+++ student/student/urls.py
@@ -23,4 +23,4 @@ from . import view, HOD_view, STAFF_view, STUDENT_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', view.INDEX, name = 'home')
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
