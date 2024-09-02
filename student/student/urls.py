f4468885b6c31b88ab2b2fb4ac84413bddf400b8
Omer Hamza <omer.hamza121996@gmail.com>
2024-09-02 16:32:51 +0500

    added requirements file

--- student/student/urls.py
+++ student/student/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 from . import view, HOD_view, STAFF_view, STUDENT_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admn/', admin.site.urls),
     path('', view.INDEX, name = 'home')
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
